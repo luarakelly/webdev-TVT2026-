3d25a9f47c203b34da9282c62bac69fc057fdc4b
luarakelly <lua.kellly@gmail.com>
2026-03-29 17:53:31 +0300

    week2, recap3, task2: Todo list with document.createElement('htmlElement').

--- week2/js/JS-recap3/dom-assignments/t2/t2.js
+++ week2/js/JS-recap3/dom-assignments/t2/t2.js
@@ -28,3 +28,34 @@ const todoList = [
 ];
 
 // add your code here
+
+// select the ul element
+const todoListElement = document.querySelector("ul");
+
+function addTodoItems() {
+  for (let todo of todoList) {
+
+    // create elements
+    const li = document.createElement("li");
+    const input = document.createElement("input");
+    const label = document.createElement("label");
+
+    // setup input
+    input.type = "checkbox";
+    input.id = `todo-${todo.id}`;
+    input.checked = todo.completed;
+
+    // setup label
+    label.htmlFor = `todo-${todo.id}`; // IMPORTANT
+    label.textContent = todo.task;
+
+    // build structure
+    li.appendChild(input);
+    li.appendChild(label);
+
+    // add to ul
+    todoListElement.appendChild(li);
+  }
+}
+
+addTodoItems();
