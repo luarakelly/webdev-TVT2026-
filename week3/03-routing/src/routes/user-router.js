c25a8dc709ce1a1e74db658bd214ceff4ce50b04
luarakelly <lua.kellly@gmail.com>
2026-04-03 03:21:38 +0300

    Add REST API with MVC structure including cats and users endpoints, controllers and models

---
+++ week3/03-routing/src/routes/user-router.js
@@ -0,0 +1,20 @@
+import express from 'express';
+import {
+  getUser,
+  getUserById,
+  postUser,
+  putUser,
+  deleteUser,
+} from '../controllers/user-controller.js';
+
+const router = express.Router();
+
+router.route('/').get(getUser).post(postUser);
+
+router
+  .route('/:id')
+  .get(getUserById)
+  .put(putUser)
+  .delete(deleteUser);
+
+export default router;
